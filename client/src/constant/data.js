e3e9671c842fadde85980d038e06fce58556ac5a
AbhinavTalesra <abhinavtalesra@gmail.com>
2021-07-07 02:54:35 +0530

    first commit

--- client/src/constant/data.js
+++ client/src/constant/data.js
@@ -1,15 +1,3 @@
-export const navData = [
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100', text: 'Top Offers' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100', text: 'Grocery' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100', text: 'Mobile' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100', text: 'Fashion' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100', text: 'Electronics' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/ee162bad964c46ae.png?q=100', text: 'Home' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100', text: 'Appliances' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100', text: 'Travel' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100', text: 'Beauty, Toys & More' }
-];
-
 export const bannerData = [
     'https://images.unsplash.com/photo-1612123912968-5f6e964e8ea5?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8dGVsZXZpc2lvbixkZXNrdG9wfHx8fHx8MTYyNTUwNzk0NA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600',
     'https://images.unsplash.com/photo-1587304947143-7389c4706d43?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8c2hvZXMsd2F0Y2h8fHx8fHwxNjI1NTA3OTk0&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600',
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,13 +3,11 @@ const router = express.Router();
 
 import { signUp, login } from './../controller/signup-controller.js';
 import { getProductById, getProducts } from './../controller/prod-controller.js';
-//import { addItemInCart } from '../controller/cart-controller.js';
 
 router.post('/signup', signUp);
 router.post('/login', login);
 router.get('/products', getProducts);
 router.get('/product/:id', getProductById);
-//router.post('/cart/add', addItemInCart);
 
 export default router;
 
